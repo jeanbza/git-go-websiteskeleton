3cdf1fa932f628e2f6ff66f767435c735b3c50d7
jadekler <jadekler@gmail.com>
2014-04-28 21:24:43 -0400

    Switched logging to glog

--- app/common/access_log.go
+++ app/common/access_log.go
@@ -3,12 +3,12 @@
 package common
 
 import (
-    "os"
-    "io"
     "time"
     "strings"
     "strconv"
     "net/http"
+
+    "github.com/golang/glog"
 )
 
 const (
@@ -45,10 +45,6 @@ func LogAccess(w http.ResponseWriter, req *http.Request, duration time.Duration)
 }
 
 func writeAccessLog(record *accessLog) {
-    logRecord := "["+record.time.Format("02/Jan/2006 03:04:05")+"] "+record.ip+" "+record.protocol+" "+record.method+": "+record.uri+" (load time: "+strconv.FormatFloat(record.elapsedTime.Seconds(), 'f', 5, 64)+" seconds)\n"
-
-    file, err := os.OpenFile(logFile, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0666)
-    CheckError(err)
-    io.WriteString(file, logRecord)
-    file.Close()
+    logRecord := "["+record.time.Format("02/Jan/2006 03:04:05")+"] "+record.ip+" "+record.protocol+" "+record.method+": "+record.uri+" (load time: "+strconv.FormatFloat(record.elapsedTime.Seconds(), 'f', 5, 64)+" seconds)"
+    glog.Infoln(logRecord)
 }
