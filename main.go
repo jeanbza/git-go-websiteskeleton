a57dfac20fa16e3b945578f5a7ba59f76d333e5a
bryfry <bryon.fryer@gmail.com>
2014-10-12 16:20:05 -0400

    simplify httpInterceptor pattern

--- main.go
+++ main.go
@@ -1,53 +1,53 @@
 package main
 
 import (
-    "net/http"
-    "time"
-    "flag"
+	"flag"
+	"net/http"
+	"time"
 
-    "git-go-websiteskeleton/app/common"
-    "git-go-websiteskeleton/app/home"
-    "git-go-websiteskeleton/app/user"
+	"git-go-websiteskeleton/app/common"
+	"git-go-websiteskeleton/app/home"
+	"git-go-websiteskeleton/app/user"
 
-    "github.com/gorilla/mux"
-    "github.com/golang/glog"
+	"github.com/golang/glog"
+	"github.com/gorilla/mux"
 )
 
-var router *mux.Router
-
 func main() {
-    flag.Parse()
-    defer glog.Flush()
+	flag.Parse()
+	defer glog.Flush()
 
-    router = mux.NewRouter()
-    http.HandleFunc("/", httpInterceptor)
+	router := mux.NewRouter()
+	http.Handle("/", httpInterceptor(router))
 
-    router.HandleFunc("/", home.GetHomePage).Methods("GET")
-    router.HandleFunc("/user{_:/?}", user.GetHomePage).Methods("GET")
+	router.HandleFunc("/", home.GetHomePage).Methods("GET")
+	router.HandleFunc("/user{_:/?}", user.GetHomePage).Methods("GET")
 
-    router.HandleFunc("/user/view/{id:[0-9]+}", user.GetViewPage).Methods("GET")
-    router.HandleFunc("/user/{id:[0-9]+}", user.GetViewPage).Methods("GET")
+	router.HandleFunc("/user/view/{id:[0-9]+}", user.GetViewPage).Methods("GET")
+	router.HandleFunc("/user/{id:[0-9]+}", user.GetViewPage).Methods("GET")
 
-    fileServer := http.StripPrefix("/static/", http.FileServer(http.Dir("static")))
-    http.Handle("/static/", fileServer)
+	fileServer := http.StripPrefix("/static/", http.FileServer(http.Dir("static")))
+	http.Handle("/static/", fileServer)
 
-    http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
 
-func httpInterceptor(w http.ResponseWriter, req *http.Request) {
-    startTime := time.Now()
+func httpInterceptor(router http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		startTime := time.Now()
 
-    router.ServeHTTP(w, req)
+		router.ServeHTTP(w, req)
 
-    finishTime := time.Now()
-    elapsedTime := finishTime.Sub(startTime)
+		elapsedTime := time.Now().Sub(startTime)
 
-    switch req.Method {
-    case "GET":
-        // We may not always want to StatusOK, but for the sake of 
-        // this example we will
-        common.LogAccess(w, req, elapsedTime)
-    case "POST":
-        // here we might use http.StatusCreated
-    }
+		switch req.Method {
+		case "GET":
+			// We may not always want to StatusOK, but for the sake of
+			// this example we will
+			common.LogAccess(w, req, elapsedTime)
+		case "POST":
+			// here we might use http.StatusCreated
+		}
+
+	})
 }
